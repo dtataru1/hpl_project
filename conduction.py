e285d143b0783e1cb3e2876c813e72b8eb93c9fc
Daniel Tataru <daniel.tataru@epfl.ch>
2022-04-14 11:03:07 +0200

    Conduction calculation

---
+++ conduction.py
@@ -0,0 +1,64 @@
+#HUM-433 Project, Energy Consumption Simulator
+#Authors: Daniel Tataru/Erwan Serandour/Romeo Tatti
+#Created on 14/04/2022
+#Simulation of heat conduction of a building through a wall
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+
+Th_cond_wall = 2.25 # W/m/K, concrecte wall themrla Conductivity
+
+#Comuting heat loss through conduction [W] based on inside and outside temperature
+def heat_loss_wall(T_out, T_in, A_wall, e_wall):
+    U_wall = Th_cond_wall/e_wall
+    Q_loss = U_wall*A_wall*(T_out-T_in)
+    return Q_loss
+
+#Computing total energy loss [J] from heat loss information
+def energy_loss_wall(Q, timestep):
+    E_loss = 0
+    for i in range(0, len(Q)-1):
+        E_loss = E_loss + (Q[i]+Q[i+1])/2 * timestep
+    return E_loss
+
+#Parameters
+
+#Reading csv temperature data
+T_file = open('geneva_temperature_2weeks.csv')
+T_csvreader = csv.reader(T_file)
+next(T_csvreader)
+T_out = []
+for row in T_csvreader:
+        T_out.append(row)
+T_out = np.reshape(np.array(T_out, dtype=np.float32), len(T_out))
+
+
+def update_plot():
+    #Constants
+    T_in = 20 #K, inside desired temperature (assumed constant)
+    A_wall = 40 #m^2, Wall surface area in contact with outside
+    timestep = 3600*24 #1-hour timestep between temperature data points
+    J2kwh = 2.77778*10**(-7) #Conversion coefficient between J and kWh
+
+    #Wall thickness (parameter to be modified)
+    e_wall = 0.3
+
+    #Running the simulation
+    Q_loss = heat_loss_wall(T_out, T_in, A_wall, e_wall)
+    E_loss = np.zeros(14)
+
+    for i in range(13):
+        E_loss[i] = (energy_loss_wall(Q_loss[i:i+2], timestep))
+    E_loss = E_loss*J2kwh
+    #plt.plot(Q_loss)
+    #plt.title('Heat loss (W) as a function of time')
+    #plt.show()
+
+    #plt.plot(E_loss)
+    #plt.title('Daily energy loss in kWh')
+    #plt.show()
+    
+
