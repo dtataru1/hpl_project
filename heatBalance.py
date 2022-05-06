20d46ed19efde650a83c509dd031ee015cd06e03
erwan serandour <erwan.serandour@epfl.ch>
2022-05-06 17:08:45 +0200

    improve interface and add aggregate

--- heatBalance.py
+++ heatBalance.py
@@ -7,6 +7,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
 
+
+
 ## TODO end implementation heat balance
 class HeatBalanceMonth():
 
@@ -32,11 +34,18 @@ class HeatBalanceGraph(FigureCanvasQTAgg):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axs = fig.add_subplot(111)
+        fig.patch.set_facecolor('#f0f0f0')
+        self.axsMonths, self.axsTot = fig.subplots(1,2, gridspec_kw={'width_ratios': [5, 1]})
         super(HeatBalanceGraph, self).__init__(fig)
     
+    def remove_frames(self, axs):
+        axs.spines['top'].set_visible(False)
+        axs.spines['right'].set_visible(False)
+
+    
     def plot(self, heatBalance):
-        self.axs.cla()
+        self.axsMonths.cla()
+        self.axsTot.cla()
         consumption = heatBalance.monthlyHeatconsumption.values()
         labels = heatBalance.monthlyHeatconsumption.keys()
         indices =  np.arange(len(labels))
@@ -49,13 +58,30 @@ class HeatBalanceGraph(FigureCanvasQTAgg):
         airCooling = extract_gain(consumption, lambda h : min(0,h.heaterGain))
         heatLoss = extract_gain(consumption, lambda h : h.heatLoss)
 
-        self.axs.bar(indices, heaterGain, color='#ef4a5a', label='chauffage')
-        self.axs.bar(indices, solarGain, bottom=heaterGain,color='#ffd11c', label='gain solaire')
-        self.axs.bar(indices, airCooling,color='#3ec1c3', label='climatisation')
-        self.axs.bar(indices, heatLoss, bottom=airCooling, color='#7e7e85', label='perte thermique')
-        self.axs.set_xticks(indices)
-        self.axs.set_xticklabels(labels)
-        self.axs.legend()
+        tot_heatLoss = [-sum(heatLoss)]
+        tot_aircooling = [-sum(airCooling)]
+        self.axsTot.bar([1],tot_aircooling, color='#3ec1c3')
+        self.axsTot.bar([1],tot_heatLoss, bottom=tot_aircooling, color='#7e7e85')
+        self.axsTot.set_title('consomation annuel')
+        self.axsTot.plot([0.5, 1.5], [149, 149], color='black', label='consomation moyenne')
+        self.axsTot.plot([0.5, 1.5], [41, 41], color='#a2c616', label='minergie')
+        self.remove_frames(self.axsTot)
+        self.axsTot.set_xticks([])
+        self.axsTot.legend()
+
+
+        self.axsMonths.bar(indices, heaterGain, color='#ef4a5a', label='chauffage')
+        self.axsMonths.bar(indices, solarGain, bottom=heaterGain,color='#ffd11c', label='gain solaire')
+        self.axsMonths.bar(indices, airCooling,color='#3ec1c3', label='climatisation')
+        self.axsMonths.bar(indices, heatLoss, bottom=airCooling, color='#7e7e85', label='perte thermique')
+        self.remove_frames(self.axsMonths)
+
+        self.axsMonths.set_xticks(indices)
+        self.axsMonths.set_xticklabels(labels)
+        self.axsMonths.set_title('consomation par mois')
+        self.axsMonths.legend()
+
+
         self.draw()
 
 def extract_gain(ls, extract):
